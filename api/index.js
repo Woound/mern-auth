4ab157c1c068fa51e8fcee098c92c9f5c928bf6d
Woound <zohaibali1546@gmail.com>
2024-05-29 05:29:17 +0100

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Conntected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error)
+  })
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
